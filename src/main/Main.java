49626412c6979b1e9d0bb5346ca82498a53e8850
sooyoung <tndud13580@naver.com>
2022-06-10 18:11:36 +0900

    1

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,21 @@
 package main;
 import java.util.Scanner;
-
+import Student1.*;
 public class Main {
 	public static void main(String[] args) {
+		
 		Scanner scan=new Scanner(System.in);
-		System.out.println("숫자를 입력하세요 >>>");
-		int user=scan.nextInt();
 		while(true) {
 			System.out.println("1. 김수영 학생의 관리이동");
 			System.out.println("2. 정광재 학생의 관리이동");
 			System.out.println("3. 유병현 학생의 관리이동");
 			System.out.println("4. 종료");
+			System.out.println("숫자를 입력하세요 >>>");
+			int user=scan.nextInt();
 			switch(user) {
 			case 1:
+				Program p = new Program();
+				break;
 			case 2:
 			case 3:
 			case 4:
