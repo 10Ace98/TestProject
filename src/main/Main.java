eb8015ffc5a53d361e2ed34cd79c5bdacabfd2ad
ybh <yoobh96@daum.net>
2022-06-10 18:01:05 +0900

    ac

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
 import java.util.Scanner;
 
+import student3.Student3;
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
 		System.out.println("숫자를 입력하세요 >>>");
 		int user=scan.nextInt();
+		Student3 s3=new Student3();
+		
 		while(true) {
 			System.out.println("1. 김수영 학생의 관리이동");
 			System.out.println("2. 정광재 학생의 관리이동");
@@ -13,9 +17,15 @@ public class Main {
 			System.out.println("4. 종료");
 			switch(user) {
 			case 1:
+				break;
 			case 2:
+				break;
 			case 3:
+				s3.start();
+				break;
 			case 4:
+				System.out.println("프로그램을 종료합니다");
+				System.exit(user);
 				default :
 				System.out.println("잘못된 입력입니다");
 			}
---
+++ src/student3/Student3.java
@@ -0,0 +1,62 @@
+package student3;
+
+import java.util.Scanner;
+
+public class Student3 {
+	Scanner scan=new Scanner(System.in);
+		
+	int user;
+	
+	public void start() {
+		while(true) {
+			System.out.println("===========    학생 관리 프로그램    =========== ");
+			System.out.println("1. 등록 2.검색 3.삭제 4.수정 5. 모든학생 출력 6.종료");
+			System.out.println("숫자 입력 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+			user=scan.nextInt();
+			switch(user) {
+			case 1:
+				insert();
+				break;
+			case 2:
+				search();
+				break;
+			case 3:
+				delete();
+				break;
+			case 4:
+				update();
+				break;
+			case 5:
+				list();
+				break;
+			case 6:
+				System.out.println("시스템을 종료합니다..");
+				System.exit(user);
+				default :
+					System.out.println("잘못된 입력입니다 !");
+			}
+		}
+	}
+	
+	public void insert() {
+		System.out.println("등록");
+	}
+	
+	public void search() {
+		System.out.println("검색");
+	}
+	
+	public void delete() {
+		System.out.println("삭제");
+	}
+	
+	public void update() {
+		System.out.println("수정");
+	}
+	
+	public void list() {
+		System.out.println("모든 출력");
+	}
+
+	
+}
